1246ca1219cbd1e49d335f993ee1ecccd266896d
박호준 <hankil417@naver.com>
2020-07-08 22:13:02 +0900

    스플래쉬 화면에서 3초간 머물다 메인으로 이동

--- app/src/main/java/com/example/listviwepractic_20200707/SplashActivity.kt
+++ app/src/main/java/com/example/listviwepractic_20200707/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.example.listviwepractic_20200707
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,20 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val myHandler = Handler()
+//핸들러 변수를 이용해서 ,3초후레 {} 내부의 내용이 실행되도록
+
+        myHandler.postDelayed({
+
+            //Intent롸 화면 이동
+            val myIntent = Intent(mContext,MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+        },3000)
+
+
 
     }
 
