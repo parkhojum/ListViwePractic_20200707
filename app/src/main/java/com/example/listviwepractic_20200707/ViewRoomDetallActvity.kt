3771c392f5cdfdd1464f11306b138928ad61f3f1
박호준 <hankil417@naver.com>
2020-07-08 20:53:27 +0900

    통째로 가져온다

--- app/src/main/java/com/example/listviwepractic_20200707/ViewRoomDetallActvity.kt
+++ app/src/main/java/com/example/listviwepractic_20200707/ViewRoomDetallActvity.kt
@@ -3,8 +3,18 @@ package com.example.listviwepractic_20200707
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.listviwepractic_20200707.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detall_actvity.*
+import kotlinx.android.synthetic.main.room_list_item.*
+import kotlinx.android.synthetic.main.room_list_item.descTxt
 
 class ViewRoomDetallActvity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detall_actvity)
+        setupEvents()
+        setValues()
+    }
     override fun setupEvents() {
 
     }
@@ -12,6 +22,11 @@ class ViewRoomDetallActvity : BaseActivity() {
     override fun setValues() {
         val receivdRoom = intent.getSerializableExtra("room") as Room
         receivdRoom.price
+
+        priceTxt.text = receivdRoom.getFormattedPrice()
+        descTxt.text = receivdRoom.description
+        addressTxt.text = receivdRoom.address
+        floorTxt.text = receivdRoom.getFormattedFloor()
     }
 
 
